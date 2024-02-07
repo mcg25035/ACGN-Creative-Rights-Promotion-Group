2927ba635ce861a85fba59ad50e5971ea97b6e4e
Taylor Tang <lv10tauren@gmail.com>
2024-02-07 20:47:14 +0800

    feat: implement spa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
 import './index.css'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    }, {
+        path: "/about_us",
+        element: <App />,
+    }, {
+        path: "/donate",
+        element: <App />,
+    }, {
+        path: "/working_project",
+        element: <App />,
+    }, {
+        path: "/join_us",
+        element: <App />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
