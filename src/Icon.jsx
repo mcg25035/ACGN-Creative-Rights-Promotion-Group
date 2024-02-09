b745707618acc5cbaa98cd9a1b1cf5c37173d9f4
Owen Liu <64713824+owenliu0924@users.noreply.github.com>
2024-02-09 23:00:54 +0800

    refactor: nav bar
    Added auto hide animation to nav bar.

--- src/Icon.jsx
+++ src/Icon.jsx
@@ -2,15 +2,11 @@ import logo from '/logo.png';
 import { Link } from "react-router-dom";
 
 function Icon(){
-    var style = {
-        height: "100%",
-        width: "auto",
-        cursor: "pointer"
-    };
+
 
     return (
-        <Link to={"/"}>
-            <img style={style} className='nav-icon' src={logo} />
+        <Link to={"/"} className="">
+            <img className='h-full' src={logo} />
         </Link>
     );
 }
