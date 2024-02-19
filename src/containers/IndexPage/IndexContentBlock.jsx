18e2fcd8cee180208fee9ef220493639689dafe1
Taylor Tang <lv10tauren@gmail.com>
2024-02-19 10:11:24 +0800

    fix index page style

--- src/containers/IndexPage/IndexContentBlock.jsx
+++ src/containers/IndexPage/IndexContentBlock.jsx
@@ -5,35 +5,35 @@ import { Link } from "react-router-dom";
 /**
  * @param {String} content
  */
-function renderContentProcess(content){
-    content = content.replaceAll(" ", "\u00A0");
-    var linesStr = content.split("\n");
-    var linesElement = [];
-    for (var i in linesStr){
-        var lineContent = linesStr[i];
-        if (!lineContent){
-            lineContent = "\u00A0";
-        }
-        linesElement.push(
-            <p key={i} className="index-content content-line">
-                {lineContent}
-            </p>
-        );
-    }
-
-    var textBlock = <div className="text-block">
-        {linesElement}
-    </div>;
-
-    return textBlock;
-}
+// function renderContentProcess(content){
+//     content = content.replaceAll(" ", "\u00A0");
+//     var linesStr = content.split("\n");
+//     var linesElement = [];
+//     for (var i in linesStr){
+//         var lineContent = linesStr[i];
+//         if (!lineContent){
+//             lineContent = "\u00A0";
+//         }
+//         linesElement.push(
+//             <p key={i} className="index-content content-line">
+//                 {lineContent}
+//             </p>
+//         );
+//     }
+//
+//     var textBlock = <div className="text-block">
+//         {linesElement}
+//     </div>;
+//
+//     return textBlock;
+// }
 
 function IndexContentBlock({ articleData }){
-    const { id, title, content, thumbnail, post_by: postBy } = articleData;
+    const { id, title, thumbnail, post_by: postBy } = articleData;
     return <div className="index-content-container">
         <div className="index-content-header">
-            <p class="date">2077.07.07</p>
-            <p class="author">{postBy}</p>
+            <p className="date">2077.07.07</p>
+            <p className="author">{postBy}</p>
         </div>
         <Link to={`/article/${id}`}>
             <div className="content-wrapper">
