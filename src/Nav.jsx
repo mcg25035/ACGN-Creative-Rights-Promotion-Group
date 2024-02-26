2a8df7bc7a1a2a6b763487cb7ea808f09ae092d8
TommcyOWO <tangminghong.tom@gmail.com>
2024-02-26 21:13:40 +0800

    rebuild Nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,8 +3,7 @@ import Link from "./Link";
 import { useState, useEffect } from "react";
 import './Nav.scss';
 
-
-function Nav(){
+function Nav() {
     const [visible, setVisible] = useState(true);
     const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
 
@@ -23,16 +22,27 @@ function Nav(){
             window.removeEventListener('scroll', handleScroll);
         };
     }, [prevScrollPos]);
-    return <div className={`bg-white top-0 fixed w-full flex px-16 items-center h-[100rem] z-[1] transform ${visible ? '' : 'translate-y-[-100%]'} transition-all duration-150 ease-out`}>
-        <li className="mr-auto"><Icon /></li>
-        <ul className="w-1/2 flex items-center justify-between">
-            <li><Link to="about_us" name="關於我們" /></li>
-            <li><Link to="donate" name="贊助支持" /></li>
-            <li><Link to="working_project" name="企劃進度" /></li>
-            <li><Link to="join_us" name="加入我們" /></li>
-            <li><Link to="login" name="Login"/></li>
-        </ul>
-    </div>;
+
+    return (
+        <div className={`bg-white top-0 fixed w-full flex px-16 items-center h-[100rem] z-[1] transform ${visible ? '' : 'translate-y-[-100%]'} transition-all duration-150 ease-out`}>
+            <li className="mr-auto"><Icon /></li>
+            <div className="menu">
+                <input type="checkbox" className="checkbox"/>
+                <div className="hamburger-lines">
+                    <span className="line l1"></span>
+                    <span className="line l2"></span>
+                    <span className="line l3"></span>
+                </div>
+                <ul className="w-1/2 flex items-center justify-between">
+                    <li><Link to="about_us" name="關於我們" /></li>
+                    <li><Link to="donate" name="贊助支持" /></li>
+                    <li><Link to="working_project" name="企劃進度" /></li>
+                    <li><Link to="join_us" name="加入我們" /></li>
+                    <li><Link to="login" name="登入"/></li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Nav;
