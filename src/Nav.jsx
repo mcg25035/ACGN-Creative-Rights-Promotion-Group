d8410fb007416debe8e9c0e2324f8f8a5db0d6fe
TommcyOWO <tangminghong.tom@gmail.com>
2024-02-28 17:20:41 +0800

    done

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,8 +4,13 @@ import { useState, useEffect } from "react";
 import './Nav.scss';
 
 function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
     const [visible, setVisible] = useState(true);
     const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     useEffect(() => {
         const handleScroll = () => {
@@ -24,23 +29,22 @@ function Nav() {
     }, [prevScrollPos]);
 
     return (
-        <div className={`bg-white top-0 fixed w-full flex px-16 items-center h-[100rem] z-[1] transform ${visible ? '' : 'translate-y-[-100%]'} transition-all duration-150 ease-out`}>
-            <li className="mr-auto"><Icon /></li>
-            <div className="menu">
-                <input type="checkbox" className="checkbox"/>
-                <div className="hamburger-lines">
+        <div className={`z-[2] bg-white top-0 fixed w-full px-16 items-center h-[100rem] transform ${ visible ? '' : 'translate-y-[-100%]' } transition-all duration-150 ease-out`}>
+            <div className="menu mr-[20px] flex justify-between items-center">
+                <span className="h-[100rem]"><Icon/></span>
+                <div className={`hamburger-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
                     <span className="line l1"></span>
                     <span className="line l2"></span>
                     <span className="line l3"></span>
                 </div>
-                <ul className="w-1/2 flex items-center justify-between">
-                    <li><Link to="about_us" name="關於我們" /></li>
-                    <li><Link to="donate" name="贊助支持" /></li>
-                    <li><Link to="working_project" name="企劃進度" /></li>
-                    <li><Link to="join_us" name="加入我們" /></li>
-                    <li><Link to="login" name="登入"/></li>
-                </ul>
             </div>
+            <ul className={`block relative bg-white p-[25px] top-1 float-end ${isOpen ? '':'hidden'}`}>
+                <li><Link to="about_us" name="關於我們" /></li>
+                <li><Link to="donate" name="贊助支持" /></li>
+                <li><Link to="working_project" name="企劃進度" /></li>
+                <li><Link to="join_us" name="加入我們" /></li>
+                <li><Link to="login" name="登入"/></li>
+            </ul>
         </div>
     );
 }
