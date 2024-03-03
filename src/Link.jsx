abcb6c45aa49737ee72bb7ea4569488c03761613
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-03-03 18:16:31 +0800

    adjust css of navbar & menu

--- src/Link.jsx
+++ src/Link.jsx
@@ -3,27 +3,32 @@ import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function Link({ name, to }){
-    var ref = React.createRef();
+function Link({ name, to, subtext }){
+    var ref = [React.createRef()];
     var onMouse = {
         over: ()=>{
-         /**@type {HTMLElement} */
-            var element = ref.current;
-
-            element.classList.add("on-hover");
+            for (var i = 0; i < ref.length; i++){
+                /**@type {HTMLElement} */
+                var element = ref[i].current;
+                if (!element) continue;
+                element.classList.add("on-hover");
+            }
         },
         out: ()=>{
-         /**@type {HTMLElement} */
-            var element = ref.current;
-
-            element.classList.remove("on-hover");
+            for (var i = 0; i < ref.length; i++){
+                /**@type {HTMLElement} */
+                var element = ref[i].current;
+                if (!element) continue;
+                element.classList.remove("on-hover");
+            }
         }
     };
 
     return (
         <div className="nav-link" onMouseOut={onMouse.out} onMouseOver={onMouse.over}>
-            <RouterLink ref={ref} to={`/${to}`} className="nav-text">
-                {name}
+            <RouterLink ref={ref[0]} to={`/${to}`} className="nav-text">
+                <p className="text-main">{name}</p>
+                {subtext && <p className="text-sub">{subtext}</p>}
             </RouterLink>
         </div>
     );
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -29,22 +29,34 @@ function Nav() {
     }, [prevScrollPos]);
 
     return (
-        <div className={`z-[2] bg-white top-0 fixed w-full px-16 items-center h-[100rem] transform ${ visible ? '' : 'translate-y-[-100%]' } transition-all duration-150 ease-out`}>
-            <div className="menu mr-[20px] flex justify-between items-center">
-                <span className="h-[100rem]"><Icon/></span>
-                <div className={`hamburger-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                    <span className="line l1"></span>
-                    <span className="line l2"></span>
-                    <span className="line l3"></span>
+        <div>
+            <div className={`bg-white z-[5] top-0 fixed w-full px-16 items-center h-[100rem] transform ${ visible ? '' : 'translate-y-[-100%]' } transition-all duration-150 ease-out`}>
+                <div className="menu mr-[20px] flex justify-between items-center">
+                    <span className="h-[100rem]"><Icon/></span>
                 </div>
+                {/* block relative bg-white p-[25px] top-1 float-end */}
             </div>
-            <ul className={`block relative bg-white p-[25px] top-1 float-end ${isOpen ? '':'hidden'}`}>
-                <li><Link to="about_us" name="關於我們" /></li>
-                <li><Link to="donate" name="贊助支持" /></li>
-                <li><Link to="working_project" name="企劃進度" /></li>
-                <li><Link to="join_us" name="加入我們" /></li>
-                <li><Link to="login" name="登入"/></li>
-            </ul>
+            <div className={`bg-transparent z-[7] top-0 fixed w-full px-16 items-center h-[100rem] transform ${ visible ? '' : 'translate-y-[-100%]' } transition-all duration-150 ease-out`}>
+                <div className="menu-transparent mr-[20px] flex justify-between items-center">
+                    <div className={`hamburger-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        <span className="line l1"></span>
+                        <span className="line l2"></span>
+                        <span className="line l3"></span>
+                    </div>
+                </div>
+                {/* block relative bg-white p-[25px] top-1 float-end */}
+            </div>
+            <ul className={`menu z-[6] ${isOpen ? '':'menu-hidden'}`}>
+                    <li className="navbar-area"></li>
+                    <li><Link to="about_us" name="關於我們" subtext="About Us" /></li>
+                    <li><Link to="donate" name="贊助支持" subtext="Donate"/></li>
+                    <li><Link to="working_project" name="企劃進度" subtext="Project Progress" /></li>
+                    <li><Link to="join_us" name="加入我們" subtext="Join Us" /></li>
+                    <li><Link to="login" name="登入" subtext="Login"/></li>
+                    <li><Link to="login" name="測試多一項" subtext="test"/></li>
+                </ul>
+            <div className={`menu-overlay z-[4] ${isOpen ? '':'menu-hidden'}`}>
+            </div>
         </div>
     );
 }
