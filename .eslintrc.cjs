206eeb2e61ae79dddc33cf55a0b595b70e537ee5
TommcyOWO <tangminghong.tom@gmail.com>
2024-03-09 19:42:02 +0800

    add EslintPluginOxlint &Oxlint&VitePluginOxlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:oxlint/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { EsLinter, linterPlugin } from "vite-plugin-linter";
+import oxlintPlugin from 'vite-plugin-oxlint'
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
     plugins: [
         react(),
+        oxlintPlugin(),
         linterPlugin({
             include: ["./src/**/*.jsx"],
             linters: [new EsLinter({ configEnv: configEnv })],
