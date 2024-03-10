1981f9ff11058817cd9745e78819ad3c492de451
TommcyOWO <tangminghong.tom@gmail.com>
2024-03-10 20:57:32 +0800

    add oxlint vite plugin path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { EsLinter, linterPlugin } from "vite-plugin-linter";
-import oxlintPlugin from 'vite-plugin-oxlint'
+import oxlintPlugin from 'vite-plugin-oxlint';
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
     plugins: [
         react(),
-        oxlintPlugin(),
+        oxlintPlugin({
+            path:'./src'
+        }),
         linterPlugin({
             include: ["./src/**/*.jsx"],
             linters: [new EsLinter({ configEnv: configEnv })],
